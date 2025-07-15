f21f56c2266333d83457873ff0be5079ed90909a
himanshumaurya-hub <himanshumaurya8066@gmail.com>
2025-07-15 20:02:43 +0530

    solution.py

--- submissions/Himanshu-Maurya/reverse-integer/solution.py
+++ submissions/Himanshu-Maurya/reverse-integer/solution.py
@@ -1,1 +1,13 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        MIN_INT, MAX_INT = -2**31, 2**31 - 1
+
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+
+        reversed_num = sign * int(str(x)[::-1])
+
+        if reversed_num < MIN_INT or reversed_num > MAX_INT:
+            return 0
+        return reversed_num
 
