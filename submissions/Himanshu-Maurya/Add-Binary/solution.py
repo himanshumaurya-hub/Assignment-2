c90835796d8d6e1a57cef8fda4e9fd2d53c93f9d
himanshumaurya-hub <himanshumaurya8066@gmail.com>
2025-07-15 20:46:06 +0530

    solution.py

--- submissions/Himanshu-Maurya/Add-Binary/solution.py
+++ submissions/Himanshu-Maurya/Add-Binary/solution.py
@@ -1,1 +1,19 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        result = []
+        i, j = len(a) - 1, len(b) - 1
+        carry = 0
+
+        while i >= 0 or j >= 0 or carry:
+            bit1 = int(a[i]) if i >= 0 else 0
+            bit2 = int(b[j]) if j >= 0 else 0
+
+            total = bit1 + bit2 + carry
+            carry = total // 2
+            result.append(str(total % 2))
+
+            i -= 1
+            j -= 1
+
+        return ''.join(reversed(result))
 
